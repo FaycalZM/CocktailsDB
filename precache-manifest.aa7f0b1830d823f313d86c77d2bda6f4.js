61d03f21e4c7ec4eb8e63ce082d26454eb520682
FaycalZM <zemmourisissy@gmail.com>
2021-10-15 21:57:52 +0100

    Updates

---
+++ precache-manifest.aa7f0b1830d823f313d86c77d2bda6f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc4bab3e8d54a5f4be2a34a24f8314f",
+    "url": "/CocktailsDB/index.html"
+  },
+  {
+    "revision": "77c4ea016bd5002808c4",
+    "url": "/CocktailsDB/static/css/main.a9666752.chunk.css"
+  },
+  {
+    "revision": "eecca93d61f9a94fdbce",
+    "url": "/CocktailsDB/static/js/2.84a8ddf5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/CocktailsDB/static/js/2.84a8ddf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c4ea016bd5002808c4",
+    "url": "/CocktailsDB/static/js/main.d80d5d64.chunk.js"
+  },
+  {
+    "revision": "cb7cbf80a077a95b9277",
+    "url": "/CocktailsDB/static/js/runtime-main.6cef1384.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/CocktailsDB/static/media/logo.f3ad7664.svg"
+  }
+]);
